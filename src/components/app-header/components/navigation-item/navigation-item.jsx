83b59fbbd0355bf24f94c40d0328229fbac5d6bc
AntoninaTrapsh <antrapsh@gmail.com>
2022-10-30 17:26:13 +0300

    fix

--- src/components/app-header/components/navigation-item/navigation-item.jsx
+++ src/components/app-header/components/navigation-item/navigation-item.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavigationItem = ({Icon, ...props}) => {
     return (
-        <li className={`${styles['navigation-item']} p-5 text`}>
+        <li className={`${styles['navigation-item']} p-5 text text_type_main-default`}>
             <Icon type={props.isActive ? 'primary' : 'secondary'} />
             <span className={`${props.isActive ? '' : 'text_color_inactive'} ml-2`}>{props.text}</span>
         </li>
