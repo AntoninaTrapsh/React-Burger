2090acbe16ebe602928e3ef3ecbc265aa3b07d2c
AntoninaTrapsh <antrapsh@gmail.com>
2022-11-04 21:24:58 +0300

    added additional types to modal & modal-overlay

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import styles from "./modal.module.css"
 import {CloseIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import ModalOverlay from "../modal-overlay/modal-overlay";
+import PropTypes from "prop-types";
 
 const Modal = (props) => {
     const modalRoot = document.getElementById("modal-root");
@@ -43,5 +44,10 @@ const Modal = (props) => {
         modalRoot
     )
 }
+
+Modal.propTypes = {
+    children: PropTypes.element.isRequired,
+    handleModalClose: PropTypes.func.isRequired
+};
 
 export default Modal;
