b7bf50fac417276b560e0741c5390f9c2b8aae17
antonina <antrapsh@gmail.com>
2022-12-08 10:47:52 +0300

    displayed current navigation-item

--- src/components/app-header/components/navigation-item/navigation-item.jsx
+++ src/components/app-header/components/navigation-item/navigation-item.jsx
@@ -1,13 +1,14 @@
 import styles from "./navigation-item.module.css";
 import React from "react";
+import {Link} from "react-router-dom";
 
 const NavigationItem = ({Icon, ...props}) => {
     return (
         <li className={`${styles['navigation-item']} p-5 text text_type_main-default`}>
-            <a href="#">
+            <Link  to={props.path}>
                 <Icon type={props.isActive ? 'primary' : 'secondary'} />
                 <span className={`${props.isActive ? '' : 'text_color_inactive'} ml-2`}>{props.text}</span>
-            </a>
+            </Link>
         </li>
     );
 };
--- src/components/app-header/components/navigation/navigation.jsx
+++ src/components/app-header/components/navigation/navigation.jsx
@@ -2,20 +2,28 @@ import styles from "./navigation.module.css";
 import NavigationItem from "../navigation-item/navigation-item";
 import {BurgerIcon, ListIcon, Logo, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import React from "react";
+import {useRouteMatch} from "react-router-dom/cjs/react-router-dom";
 
 const Navigation = () => {
+
+    const isConstructor = !!useRouteMatch({ path: '/', exact: true});
+    const isFeed = !!useRouteMatch('/feed');
+    const isProfile = !!useRouteMatch('/profile');
+
     return (
         <nav>
             <ul className={`${styles['navigation']} pt-4 pb-4`}>
                 <div className={`${styles['navigation__order']}`}>
                     <NavigationItem
-                        isActive={true}
                         Icon={BurgerIcon}
+                        isActive={isConstructor}
+                        path="/"
                         text="Конструктор"
                     />
                     <NavigationItem
                         Icon={ListIcon}
-                        isActive={false}
+                        isActive={isFeed}
+                        path="/orders"
                         text="Лента заказов"
                     />
                 </div>
@@ -25,7 +33,8 @@ const Navigation = () => {
                 <div className={`${styles['navigation__profile']}`}>
                     <NavigationItem
                         Icon={ProfileIcon}
-                        isActive={false}
+                        isActive={isProfile}
+                        path="/profile"
                         text="Личный кабинет"
                     />
                 </div>
