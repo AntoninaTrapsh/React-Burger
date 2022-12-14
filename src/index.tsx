803d21de6a5fa392fb8bc79e52cd3895c787943d
antonina <antrapsh@gmail.com>
2022-12-14 13:45:02 +0300

    changed jsx to tsx file + added config

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import {Provider} from "react-redux";
 import {store} from "./services/store/store";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root')
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <Router>
